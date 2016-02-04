90f47c150b9935073b108f3884d7baf47188309f
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2016-02-04 09:20:13 -0800

    adding project

---
+++ trip-circuit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trip-circuit
+//
+//  Created by Vijay Selvaraj on 2/4/16.
+//  Copyright © 2016 Vijay Selvaraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
